a8a62fda4a9c3153174bf34ccd0d9ed825d35141
Gaurav Sahu <itsgauravsahu7+github@gmail.com>
2024-09-12 20:19:22 +0530

    v2 share

--- app/(shared)/share/[slug]/page.tsx
+++ app/(shared)/share/[slug]/page.tsx
@@ -5,12 +5,7 @@ import Link from "next/link";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const ShraredPage = async ({ params }: { params: { slug: string } }) => {
   const { page, pageLinks } = await getPageBySlugWithPageLinks(params.slug);
@@ -19,16 +14,25 @@ const ShraredPage = async ({ params }: { params: { slug: string } }) => {
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-100 to-gray-200 flex items-center justify-center p-4">
-      <Card className="w-full max-w-md">
+      <div
+        className="absolute inset-0 bg-cover bg-center z-0 filter blur-lg "
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1575995872537-3793d29d972c?q=80&w=1448&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+        }}
+      ></div>
+      <div className="absolute inset-0 bg-black opacity-20 pointer-events-none mix-blend-overlay"></div>
+      <Card className="w-full max-w-md z-10">
         <CardHeader className="text-center">
           <Avatar className="w-24 h-24 mx-auto mb-4">
             <AvatarImage
-              src="/placeholder.svg?height=96&width=96"
+              className="object-cover"
+              src="https://images.unsplash.com/photo-1579762715118-a6f1d4b934f1?q=80&w=1368&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="Profile Picture"
             />
-            <AvatarFallback>JD</AvatarFallback>
+            <AvatarFallback>{page.name}</AvatarFallback>
           </Avatar>
-          <CardTitle className="text-2xl font-bold">{page.name}</CardTitle>
+          <CardTitle className="text-2xl font-bold">@{page.name}</CardTitle>
         </CardHeader>
         <CardContent className="space-y-4">
           <p className="text-center text-sm text-gray-600">
